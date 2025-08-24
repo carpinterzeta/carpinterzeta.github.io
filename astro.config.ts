853f920bcda6e97230155fb1f2bb176ba5dfa533
carpinterzeta <albcarpintero@gmail.com>
2025-08-25 00:16:00 +0200

    update correct site name

--- astro.config.ts
+++ astro.config.ts
@@ -115,7 +115,7 @@ export default defineConfig({
   // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
   // ! Please remember to replace the following site property with your own domain
-  site: "https://carpinterzeta.github.io",
+  site: "https://krpntrz.xyz",
   vite: {
     build: {
       sourcemap: true, // Source maps generation
